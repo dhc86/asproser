511ce718eaf5b935ea695eabbfd0ebbb2cfd7b19
DIEGO <diegoh_c@yahoo.com>
2016-01-29 14:27:43 -0800

    carousel for most popular products at ther bottom of landing page implemented

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :products
 
-  has_attached_file :category_image, styles: { category_index: "300x300>", category_show: "400x400>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :category_image, styles: { category_index: "700x450>", category_show: "400x400>" }, default_url: "/images/:style/missing.png"
 validates_attachment_content_type :category_image, content_type: /\Aimage\/.*\Z/
 end
